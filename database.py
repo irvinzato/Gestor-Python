f4b427f220c04253c7da685c56f62889c5627878
irvinzato <multizato@hotmail.com>
2023-04-06 17:56:44 -0600

    Añadi csv especifico para las pruebas unitarias, archivo de configuraciones para saber cual tomar

--- database.py
+++ database.py
@@ -7,8 +7,17 @@ class Cliente:
     def __str__(self):
         return f"({self.ine}) {self.nombre} {self.apellido}"
     
+import csv
+import config #Para poder separar el programa con las pruebas unitarias
+
 class Clientes:
     lista = []
+    #Para implementar persistencia(Todo quedara dentro del csv)
+    with open(config.DATABASE_PATH, newline='\n') as fichero:
+        reader = csv.reader(fichero, delimiter=';')
+        for ine, nombre, apellido in reader:
+            cliente = Cliente(ine, nombre, apellido)
+            lista.append(cliente)
 
     @staticmethod
     def buscar(ine):
@@ -20,6 +29,7 @@ class Clientes:
     def crear(ine, nombre, apellido):
         cliente = Cliente(ine, nombre, apellido)
         Clientes.lista.append(cliente)
+        Clientes.guardar_en_csv()
         return cliente
 
     @staticmethod
@@ -27,11 +37,21 @@ class Clientes:
         for indice,cliente in enumerate(Clientes.lista):
             if cliente.ine == ine:
                 Clientes.lista[indice].nombre   = nombre
-                Clientes.lista[indice].apellido = apellido 
+                Clientes.lista[indice].apellido = apellido
+                Clientes.guardar_en_csv()
                 return Clientes.lista[indice]
 
     @staticmethod
     def borrar(ine):
         for indice,cliente in enumerate(Clientes.lista):
             if cliente.ine == ine:
-                return Clientes.lista.pop(indice)
+                cliente_recuperado = Clientes.lista.pop(indice)
+                Clientes.guardar_en_csv()
+                return cliente_recuperado
+
+    @staticmethod        
+    def guardar_en_csv():
+        with open(config.DATABASE_PATH, 'w', newline='\n') as fichero:
+            writer = csv.writer(fichero, delimiter=';')
+            for cliente in Clientes.lista:
+                writer.writerow((cliente.ine, cliente.nombre, cliente.apellido)) #writerow recibe una tupla
--- tests/test_database.py
+++ tests/test_database.py
@@ -2,6 +2,8 @@ import unittest
 import database as db
 import helpers
 import copy
+import config
+import csv
 
 #Utilice el paquete "pytest" para utilizar las pruebas "pytest -v" comprueba en el directorio de pruebas los tests
 class TestDataBase(unittest.TestCase):
@@ -10,7 +12,7 @@ class TestDataBase(unittest.TestCase):
         db.Clientes.lista = [ db.Cliente("27S", "Irving", "Rivera"), db.Cliente("52L", "Angeles", "Lopez"),
                               db.Cliente("34R", "Jesus" , "Rivera"), db.Cliente("47W", "Laura"  , "Lopez") ]
 
-    ### Validaciones de "database" ###
+    ### TEST de "database" ###
     def test_buscar_cliente(self):
         cliente_existente   = db.Clientes.buscar("27S")
         cliente_inexistente = db.Clientes.buscar("69S")
@@ -37,8 +39,22 @@ class TestDataBase(unittest.TestCase):
         self.assertIsNone(buscar_cliente)
 
 
-    ### Validaciones de "helpers" ###
+    ### TEST de "helpers" ###
     def test_ine_valido(self):
         self.assertTrue(helpers.ine_valido('10A', db.Clientes.lista))
         self.assertFalse(helpers.ine_valido('27S', db.Clientes.lista))
         self.assertFalse(helpers.ine_valido('aaa', db.Clientes.lista))
+
+    ### TEST para probar el archivo "csv" de las pruebas unitarias
+    def test_escritura_csv(self):
+        db.Clientes.borrar("27S")
+        db.Clientes.borrar("52L")
+        db.Clientes.modificar("34R", "Jose Mar", "Rivera")
+
+        ine, nombre, apellido = None, None, None
+        with open(config.DATABASE_PATH, newline='\n') as fichero:
+            reader = csv.reader(fichero, delimiter=';')
+            ine, nombre, apellido = next(reader) #Saco al siguiente del fichero
+        self.assertEqual(ine, "34R")
+        self.assertEqual(nombre, "Jose Mar")
+        self.assertEqual(apellido, "Rivera")
