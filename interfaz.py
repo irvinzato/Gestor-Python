db38aaeef590aeea57552d795e72e967e010c78e
irvinzato <multizato@hotmail.com>
2023-04-06 21:37:15 -0600

    Agregue sub ventana para crear registro y funcionalidad de borrar registro

--- interfaz.py
+++ interfaz.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter.messagebox import askokcancel, WARNING
 import database as db
 
 #Un mixin es una clase que contiene una o varias definiciones y podemos heredarlas en otras clases
@@ -15,6 +16,47 @@ class CentrarVentanaMixin:
         self.geometry(f"{w}x{h}+{x}+{y}") #WIDTHxHEIGHT+OFFSET_X+OFFSET_Y
 
 
+class VentanaCrearCliente(Toplevel, CentrarVentanaMixin):
+    def __init__(self, padre):
+        super().__init__(padre) #Llamo al constructor de la clase heredada(Toplevel)
+        self.title("Crear cliente")
+        self.build()
+        self.centrar_ventana()
+        #Para impedir hacer acción en la otra ventana si tenemos esta abierta "transient" y "grab_set"
+        self.transient(padre)
+        self.grab_set()
+
+    def build(self):
+        marco = Frame(self) #Aquí si pide el self, para que el frame lo ponga dentro de "VentanaCrearCliente"
+        marco.pack(padx=20, pady=10)
+
+        Label(marco, text="INE (2 enteros y 1 upper char)").grid(row=0, column=0)
+        Label(marco, text="Nombre (2 a 30 carácteres)").grid(row=0, column=1)
+        Label(marco, text="Apellido (2 a 30 carácteres)").grid(row=0, column=2)
+
+        ine = Entry(marco)
+        ine.grid(row=1, column=0)
+        nombre = Entry(marco)
+        nombre.grid(row=1, column=1)
+        apellido = Entry(marco)
+        apellido.grid(row=1, column=2)
+
+        marco_2 = Frame(self)
+        marco_2.pack(pady=10)
+
+        crear = Button(marco_2, text="Crear", command=self.crear_cliente)
+        crear.configure(state=DISABLED)
+        crear.grid(row=0, column=0)
+        Button(marco_2, text="Cancelar", command=self.cerrar).grid(row=0, column=1)
+
+    def crear_cliente(self):
+        pass
+
+    ### Para cerrar un "Toplevel" ###
+    def cerrar(self):
+        self.destroy()
+        self.update()
+
 class MainWindow(Tk, CentrarVentanaMixin):
     def __init__(self):
         super().__init__() #Llamo al constructor de la clase heredada(Tk)
@@ -23,7 +65,8 @@ class MainWindow(Tk, CentrarVentanaMixin):
         self.centrar_ventana()
 
     def build(self):
-        marco = Frame(self)#Hereda de self porque tiene que insertarlo en la propia "MainWindow"
+                #Curiosamente tambien trabaja aunque Frame() no tenga dentro "self"
+        marco = Frame(self) #Hereda de self porque tiene que insertarlo en la propia "MainWindow"
         marco.pack()
 
         treeview = ttk.Treeview(marco)
@@ -51,15 +94,29 @@ class MainWindow(Tk, CentrarVentanaMixin):
 
         treeview.pack()
 
-        frame = Frame(self)
-        frame.pack(pady=20)
+        marco_2 = Frame(self)
+        marco_2.pack(pady=20)
 
-        Button(frame, padx=20, bd=5, text="Crear", command=None).grid(row=0, column=0)
-        Button(frame, padx=20, bd=5, text="Modificar", command=None).grid(row=0, column=1)
-        Button(frame, padx=20, bd=5, text="Borrar", command=None).grid(row=0, column=2)
+        Button(marco_2, padx=20, bd=5, text="Crear", command=self.crear).grid(row=0, column=0)
+        Button(marco_2, padx=20, bd=5, text="Modificar", command=None).grid(row=0, column=1)
+        Button(marco_2, padx=20, bd=5, text="Borrar", command=self.borrar).grid(row=0, column=2) #Al llamar el metodo aqui no se usa con "()"
 
         self.treeview = treeview #Para poder tener acceso a el en los demas métodos
 
+    def borrar(self):
+        cliente = self.treeview.focus() #focus regresa donde tenemos el "foco" en el "treeview"
+        if cliente:
+            campos = self.treeview.item(cliente, "values") #Extraera los valores del cliente seleccionado en el treeview
+            confirmar = askokcancel(
+                title="Confirmar borrado",
+                message=f"¿Borrar {campos[1]} {campos[2]}?", #Recupere los valores del cliente en una lista
+                icon=WARNING
+            )
+            if confirmar:
+                self.treeview.delete(cliente)
+
+    def crear(self):
+        VentanaCrearCliente(self)
 
 if __name__ == "__main__":
     app = MainWindow()
