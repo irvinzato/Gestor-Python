c14673073f2ae876dea9e8a5be4fd99da4fa30b6
irvinzato <multizato@hotmail.com>
2023-04-09 11:04:58 -0600

    Añadi opciones para correr gestor con interfaz o con terminal

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
 import menu
+import sys
+import interfaz
 
+#Doy posibilidades de entrar en modo terminal o modo interfaz a mi aplicación
 if __name__ == "__main__":
-    menu.iniciar()
+    if len(sys.argv) > 1 and sys.argv[1] == "-t":
+        menu.iniciar()
+    else:
+        app = interfaz.MainWindow()
+        app.mainloop()
