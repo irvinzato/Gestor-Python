755eaca2da9e0161c08df29b8b7ee0cadb727d6b
irvinzato <multizato@hotmail.com>
2023-04-13 10:09:22 -0600

    Añadi modos terminal e interfaz para ocupar en run.py

--- run.py
+++ run.py
@@ -7,5 +7,6 @@ if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "-t":
         menu.iniciar()
     else:
+        print("********** ", sys.argv[0])
         app = interfaz.MainWindow()
         app.mainloop()
